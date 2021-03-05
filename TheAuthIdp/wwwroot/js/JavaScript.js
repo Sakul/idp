c55f95d2b5e6c8f8ced23005192657f09123996d
Chokcha Homsombat <48384299+chokchai9900@users.noreply.github.com>
2021-03-05 15:00:08 +0700

    added condition for receive message

--- TheAuthIdp/wwwroot/js/JavaScript.js
+++ TheAuthIdp/wwwroot/js/JavaScript.js
@@ -2,7 +2,10 @@
 
 var connection = new signalR.HubConnectionBuilder().withUrl("/sendstatus",{ accessTokenFactory: () => this.loginToken }).build();
 
-document.getElementById("mybut").disabled = true;
+document.getElementById("complete").disabled = true;
+document.getElementById("complete").style.display = "none";
+document.getElementById("fail").style.display = "none";
+document.getElementById("buttonaera").style.display = "none";
 
 connection.on("ReceiveMessage", function (message) {
     var msg = message.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;");
@@ -12,6 +15,13 @@ connection.on("ReceiveMessage", function (message) {
     if (encodedMsg != null) {
         alert(encodedMsg);
         console.log(encodedMsg);
+
+        var validate = (encodedMsg == "complete" )
+        if (validate) {
+            document.getElementById("complete").click();
+        } else {
+            document.getElementById("fail").click();
+        }
     }
     else {
         alert("Something wrong. ");
@@ -20,12 +30,13 @@ connection.on("ReceiveMessage", function (message) {
     }
 });
 connection.start().then(function () {
-    document.getElementById("mybut").disabled = false;
+    document.getElementById("complete").disabled = false;
     connection.invoke('getConnectionId')
         .then(function (connectionId) {
             console.log("connectionID : " + connectionId);
             $("#signalRconnectionId").attr("value", connectionId);
         });
+    
 }).catch(function (err) {
     return console.error(err.toString());
 });

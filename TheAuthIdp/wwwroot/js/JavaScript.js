233d7073601b7ddfdee52dc425352595a9c157f2
Anawat <anawat@perfenterprise.com>
2021-03-05 15:20:20 +0700

    remove full url qr
    Signed-off-by: Anawat <anawat@perfenterprise.com>

--- TheAuthIdp/wwwroot/js/JavaScript.js
+++ TheAuthIdp/wwwroot/js/JavaScript.js
@@ -37,7 +37,6 @@ connection.start().then(function () {
             $("#signalRconnectionId").attr("value", connectionId);
             var qrGeneratorUrl = "https://mana-facing-devtesting.azurewebsites.net/qr?t=";
             var shortBaseUrl = "https://s.manal.ink/auth/visit";
-            var fullBaseUrl = "https://mana-facing-devtesting.azurewebsites.net/auth/visit/nxxxyyy-123456?cid=client123";
             var endpoint1 = "nxxxyyy-000001";
             var endpoint2 = "nxxxyyy-000002";
             var endpoint3 = "nxxxyyy-000003";
@@ -46,9 +45,6 @@ connection.start().then(function () {
             $("#qr1").prop("src", qrGeneratorUrl + shortBaseUrl + '%2F' + endpoint1 + '%3F' + cid + '%26' + rba);
             $("#qr2").prop("src", qrGeneratorUrl + shortBaseUrl + '%2F' + endpoint2 + '%3F' + cid + '%26' + rba);
             $("#qr3").prop("src", qrGeneratorUrl + shortBaseUrl + '%2F' + endpoint3 + '%3F' + cid + '%26' + rba);
-            $("#qrf1").prop("src", qrGeneratorUrl + fullBaseUrl + '%2F' + endpoint1 + '%3F' + cid + '%26' + rba);
-            $("#qrf2").prop("src", qrGeneratorUrl + fullBaseUrl + '%2F' + endpoint2 + '%3F' + cid + '%26' + rba);
-            $("#qrf3").prop("src", qrGeneratorUrl + fullBaseUrl + '%2F' + endpoint3 + '%3F' + cid + '%26' + rba);
         });
     
 }).catch(function (err) {

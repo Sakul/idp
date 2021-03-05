5de41b6abef72f47c68f6cbf851664d5c689f60d
Anawat <anawat@perfenterprise.com>
2021-03-05 15:14:53 +0700

    generate qr from connection id
    Signed-off-by: Anawat <anawat@perfenterprise.com>

--- TheAuthIdp/wwwroot/js/JavaScript.js
+++ TheAuthIdp/wwwroot/js/JavaScript.js
@@ -1,6 +1,6 @@
 ﻿"use strict";
 
-var connection = new signalR.HubConnectionBuilder().withUrl("/sendstatus",{ accessTokenFactory: () => this.loginToken }).build();
+var connection = new signalR.HubConnectionBuilder().withUrl("/sendstatus", { accessTokenFactory: () => this.loginToken }).build();
 
 document.getElementById("mybut").disabled = true;
 
@@ -25,6 +25,20 @@ connection.start().then(function () {
         .then(function (connectionId) {
             console.log("connectionID : " + connectionId);
             $("#signalRconnectionId").attr("value", connectionId);
+            var qrGeneratorUrl = "https://mana-facing-devtesting.azurewebsites.net/qr?t=";
+            var shortBaseUrl = "https://s.manal.ink/auth/visit";
+            var fullBaseUrl = "https://mana-facing-devtesting.azurewebsites.net/auth/visit/nxxxyyy-123456?cid=client123";
+            var endpoint1 = "nxxxyyy-000001";
+            var endpoint2 = "nxxxyyy-000002";
+            var endpoint3 = "nxxxyyy-000003";
+            var cid = "cid=" + connectionId;
+            var rba = "rba=false";
+            $("#qr1").prop("src", qrGeneratorUrl + shortBaseUrl + '%2F' + endpoint1 + '%3F' + cid + '%26' + rba);
+            $("#qr2").prop("src", qrGeneratorUrl + shortBaseUrl + '%2F' + endpoint2 + '%3F' + cid + '%26' + rba);
+            $("#qr3").prop("src", qrGeneratorUrl + shortBaseUrl + '%2F' + endpoint3 + '%3F' + cid + '%26' + rba);
+            $("#qrf1").prop("src", qrGeneratorUrl + fullBaseUrl + '%2F' + endpoint1 + '%3F' + cid + '%26' + rba);
+            $("#qrf2").prop("src", qrGeneratorUrl + fullBaseUrl + '%2F' + endpoint2 + '%3F' + cid + '%26' + rba);
+            $("#qrf3").prop("src", qrGeneratorUrl + fullBaseUrl + '%2F' + endpoint3 + '%3F' + cid + '%26' + rba);
         });
 }).catch(function (err) {
     return console.error(err.toString());

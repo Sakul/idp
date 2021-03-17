9c6d446085382b8b1cd33591de3e5c95173a96cd
Sakul <miolynet@perfenterprise.com>
2021-03-17 22:04:49 +0700

    Update the way to get QrUrl & DeepLink

--- TheAuthIdp/wwwroot/js/JavaScript.js
+++ TheAuthIdp/wwwroot/js/JavaScript.js
@@ -10,62 +10,60 @@ connection.start()
                 var svcId = document.getElementById("hide-svcid").textContent;
                 var flowId = document.getElementById("hide-flowid").textContent;
                 $("#loading-session").prop("hidden", true);
+
+                // Get QrUrl & DeepLinkUrl
                 connection
                     .invoke("GetLoginUrl", svcId, flowId, connectionId)
-                    .then(function (url) {
+                    .then(function (rsp) {
+
+                        // Setup display
                         $("#login-session-heading").prop("hidden", false);
-                        if (url == "") {
+                        if (rsp === null) {
                             $("#login-session-failed").prop("hidden", false);
                         }
                         else {
                             $("#login-session-success").prop("hidden", false);
-                            $("#qr1").prop("src", url);
+                            $("#qr1").prop("src", rsp.QrUrl);
 
                             if ((/Mobi|Android/i.test(navigator.userAgent)) || /Mobi|iPad|iPhone|iPod/i.test(navigator.userAgent)) {
                                 $("#applink").prop("hidden", false);
                             }
                         }
+
+                        // Setup links
+                        var appstorelink = "itms://apps.apple.com/th/app/mana/id1273112680";
+                        var playstore_deeplink = "market://details?id=thes.mana.client";
+                        var playstorelink = "https://play.google.com/store/apps/details?id=thes.mana.client";
+                        if ((/Mobi|Android/i.test(navigator.userAgent)) || /Mobi|iPad|iPhone|iPod/i.test(navigator.userAgent)) {
+                            $("#universalUri").click(function () {
+                                var app = {
+                                    launchApp: function () {
+                                        window.location.href = rsp.LinkUrl;
+                                        setTimeout(this.openWebApp, 1000);
+                                    },
+
+                                    openWebApp: function () {
+                                        if ((/Mobi|Android/i.test(navigator.userAgent))) {
+                                            window.location.href = playstore_deeplink;
+                                        }
+                                        if (/Mobi|iPad|iPhone|iPod/i.test(navigator.userAgent)) {
+                                            window.location.href = appstorelink;
+                                        }
+                                    }
+                                };
+
+                                app.launchApp();
+                            });
+                        }
+                        else {
+                            $("#storelink").prop("hidden", false);
+                            $("#playstore").prop("href", playstorelink);
+                            $("#appstore").prop("href", appstorelink);
+                        }
                     })
                     .catch(function (err) {
                         console.error(err.toString());
                     });
-
-                var appstorelink = "itms://apps.apple.com/th/app/mana/id1273112680";
-                var playstore_deeplink = "market://details?id=thes.mana.client";
-                var playstorelink = "https://play.google.com/store/apps/details?id=thes.mana.client";
-                if ((/Mobi|Android/i.test(navigator.userAgent)) || /Mobi|iPad|iPhone|iPod/i.test(navigator.userAgent)) {
-                    $("#universalUri").click(function () {
-                        var app = {
-                            launchApp: function () {
-                                var endpointId = "nidplog-000001";
-                                var clientIdQry = "cId=" + connectionId;
-                                var svcIdQry = "svcId=" + svcId;
-                                var flowIdQry = "flowId=" + flowId;
-                                var QUESTION = '%3F';
-                                var AND = '%26';
-                                var endpointParams = endpointId + QUESTION + clientIdQry + AND + svcIdQry + AND + flowIdQry;
-                                window.location.href = 'manarising://link?np=' + endpointParams;
-                                setTimeout(this.openWebApp, 1000);
-                            },
-
-                            openWebApp: function () {
-                                if ((/Mobi|Android/i.test(navigator.userAgent))) {
-                                    window.location.href = playstore_deeplink;
-                                }
-                                if (/Mobi|iPad|iPhone|iPod/i.test(navigator.userAgent)) {
-                                    window.location.href = appstorelink;
-                                }
-                            }
-                        };
-
-                        app.launchApp();
-                    });
-                }
-                else {
-                    $("#storelink").prop("hidden", false);
-                    $("#playstore").prop("href", playstorelink);
-                    $("#appstore").prop("href", appstorelink);
-                }
             });
     })
     .catch(function (e) {
